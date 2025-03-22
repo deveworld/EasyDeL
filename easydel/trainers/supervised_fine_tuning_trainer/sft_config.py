7e910aaf5dfd93b6c9ab5cd05578ec241f0640c5
Dev world <48672862+deveworld@users.noreply.github.com>
2025-03-23 02:42:57 +0900

    Update sft_config.py

--- easydel/trainers/supervised_fine_tuning_trainer/sft_config.py
+++ easydel/trainers/supervised_fine_tuning_trainer/sft_config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import typing as tp
-from dataclasses import field
+from dataclasses import dataclass, field
 
 from easydel.utils import traversals as etr
 from easydel.utils.compiling_utils import hash_fn
